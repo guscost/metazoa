4258a4e64f0945d6486e46af38a8b5f74ca5d55e
Gus Cost <guscost@gmail.com>
2017-06-21 08:50:34 -0400

    code: proof of concept

---
+++ metazoa.js
@@ -0,0 +1,31 @@
+/**
+ * metazoa v0.1.0
+ * MIT License
+ * Copyright 2017 Gus Cost
+ */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['protozoa'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory(require('protozoa'));
+  } else {
+    root.metazoa = factory(root.protozoa);
+  }
+}(this, function (protozoa) {
+
+  // Recursive kernel describes what to do with nested values or templates
+  var _kernel = function (node, children) {
+    return children.map(function (child) {
+      var _child = protozoa(child); // Recurse through the tree!
+      if (child.ref) { node[child.ref] = _child; }
+      node.appendChild(protozoa("TEST|"));
+      return node.appendChild(_child);
+    })
+  };
+
+  return function (tmpl) {
+    tmpl.kernel = _kernel;
+    return protozoa(tmpl);
+  }
+
+}));
