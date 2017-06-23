d9a9feb55040da4cbbd148278259ad37306aed2d
Gus Cost <guscost@gmail.com>
2017-06-23 03:16:50 -0400

    code: so nice!

--- index.tsx
+++ index.tsx
@@ -4,5 +4,10 @@
 // However you can also just grab it from the global scope
 var metazoa: any = metazoa || {};
 
-var m = metazoa(<div style="font-size:32px">METAZOA</div>);
+var m = metazoa(<ul style="font-size:32px">
+  <li>METAZOA</li>
+  <li>is</li>
+  <li>working</li>
+  <li>now!</li>
+</ul>);
 document.body.appendChild(m);
