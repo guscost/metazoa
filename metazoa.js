1c560f82d7e4157659bd0b5fdfeb964d955ad16f
Gus Cost <guscost@gmail.com>
2017-07-04 13:57:16 -0400

    code: update protozoa version

--- metazoa.js
+++ metazoa.js
@@ -1,5 +1,5 @@
 /**
- * metazoa v0.1.1
+ * metazoa v0.2.1
  * MIT License
  * Copyright 2017 Gus Cost
  */
@@ -15,12 +15,20 @@
 
   // Recursive kernel describes what to do with nested values or templates
   var _kernel = function (node, children) {
-    return children.map(function (child) {
-      var _child = protozoa(child); // Recurse through the tree!
+    // Helper to recurse through the tree!
+    function recurse(child) {
+      var _child = protozoa(child);
       if (child.ref) { node[child.ref] = _child; }
       node.appendChild(protozoa("TEST|"));
       return node.appendChild(_child);
-    })
+    }
+    // `children` can be an array of nested templates
+    if (Array.isArray(children)) {
+      return children.map(function (child) { return recurse(child); });
+    // Or a single template object, or any other value
+    } else {
+      return recurse(children);
+    }
   };
 
   return function (tmpl) {
